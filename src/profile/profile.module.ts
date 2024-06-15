628ed2b0bd5b35c0575f2e42dd929bd27ec08b44
Shashank Gupta <shashankgpt270@gmail.com>
2024-06-16 00:37:01 +0530

    removed unnecessary import

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -3,17 +3,11 @@ import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema, User } from '../auth/schemas/auth.schema';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from '../auth/constants';
 import { JwtStrategy } from 'src/jwt/jwt.strategy';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '3600s' },
-    }),
   ],
   controllers: [ProfileController],
   providers: [ProfileService, JwtStrategy],
