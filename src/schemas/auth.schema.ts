d669ffd3fe89d7adfe6d7ce0fe93591d7de58930
Shashank Gupta <shashankgpt270@gmail.com>
2024-06-16 22:17:08 +0530

    document suggestion added

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -13,6 +13,20 @@ export class User {
 
   @Prop({ required: true })
   password: string;
+
+  /*
+  @Prop({ required: true })
+  role: string;
+
+  @Prop({ required: true })
+  isActive: boolean;
+
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+  */
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
